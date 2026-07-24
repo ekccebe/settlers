b64f781d88155aa82d132502de7154faa40cfdf1
Ekccebe <ekccebetaren@gmail.com>
2026-07-24 10:39:45 +0200

    added save activity list on client start

--- client/settlers_app.js
+++ client/settlers_app.js
@@ -1,25 +1,8 @@
 // use addMenuItem from main script
 addToolsMenuItem("Settlers App", ExampleMenuHandler);
 
-function ExampleMenuHandler(event)
+function SendUserListThroughAPI()
 {
-	// close all modals
-	$( "div[role='dialog']:not(#exampleModal):visible").modal("hide");
-	// create modal
-	createModalWindow('exampleModal', 'Example modal window');
-	// fill modal data 
-	$('#exampleModalData').html(ExampleMakeModal());
-	// bind buttons
-	$('#exampleModalData .showlevelbtn').click(ex_get_level);
-	// show modal
-	$('#exampleModal:not(:visible)').modal({backdrop: "static"});
-}
-
-
-function ExampleMakeModal()
-{
-
-  var out = '<div class="container-fluid"><H3>Saving guild member list. Done!</H3>';
   var guild = swmmo.application.mGameInterface.GetCurrentPlayerGuild();
   var guild_users = []
   const datetime = new Date().toISOString()
@@ -42,11 +25,48 @@ function ExampleMakeModal()
     xhr.send(JSON.stringify(guild_users));
 
   } catch (error) {
-    out += error
+    //debug("Xhr error");
   }
-	
-  out += '</div>'
-  return out;
+}
+
+$(document).ready(function () {
+
+    const timer = setInterval(function () {
+
+        if (
+            window.swmmo &&
+            swmmo.application &&
+            swmmo.application.mGameInterface &&
+            swmmo.application.mGameInterface.GetCurrentPlayerGuild
+        ) {
+            clearInterval(timer);
+            SendUserListThroughAPI();
+        }
+
+    }, 5000);
+
+});
+
+
+function ExampleMenuHandler(event)
+{
+	// close all modals
+	$( "div[role='dialog']:not(#exampleModal):visible").modal("hide");
+	// create modal
+	createModalWindow('exampleModal', 'Example modal window');
+	// fill modal data 
+	$('#exampleModalData').html(ExampleMakeModal());
+	// bind buttons
+	$('#exampleModalData .showlevelbtn').click(ex_get_level);
+	// show modal
+	$('#exampleModal:not(:visible)').modal({backdrop: "static"});
+}
+
+
+function ExampleMakeModal()
+{
+  SendUserListThroughAPI();
+  return '<div class="container-fluid"><H3>User list saved</H3></div>';
 }
 
 
