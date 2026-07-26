49a9b7179e4ff0cb4e55b319a6d8b0376b98e9b6
Ekccebe <ekccebetaren@gmail.com>
2026-07-26 21:16:19 +0200

    fixed size limit for json data

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require("cors")
 
 
 const app = express()
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 var corsMiddleware = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
